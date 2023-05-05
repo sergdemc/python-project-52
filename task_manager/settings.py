6aacca660b67251eb74e8c3d5f9ecb2defdf534b
Sergei Demchenko <hb13718@gmail.com>
2023-05-05 17:36:32 +0300

    Add noqa: 401

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 from django.contrib.messages import constants as messages
-import dj_database_url
+import dj_database_url  # noqa: 401
 import rollbar
 import os
 
