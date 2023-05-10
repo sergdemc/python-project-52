3fb3869e0b17750fc38027027e7423b4dc8c7f80
Sergei Demchenko <hb13718@gmail.com>
2023-05-10 12:05:17 +0300

    Add full path for auth migrations

--- users/migrations/0001_initial.py
+++ users/migrations/0001_initial.py
@@ -11,7 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        ('auth.migrations', '0013_alter_user_options'),
+        ('django.contrib.auth', '0013_alter_user_options'),
     ]
 
     operations = [
