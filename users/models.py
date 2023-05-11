602b8a1c457b2c85f03567aa39ef1e5f4b66e116
Sergei Demchenko <hb13718@gmail.com>
2023-05-11 08:55:32 +0300

    Add __str__ for User model

--- users/models.py
+++ users/models.py
@@ -2,5 +2,9 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+
+    def __str__(self):
+        return self.get_full_name()
+
     class Meta:
         ordering = ['date_joined']
